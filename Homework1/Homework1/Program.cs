cac2c0d775e2ceaf0317fe5a2267e63b6876fc20
vera1003 <825238453@qq.com>
2018-09-12 16:06:50 +0800

    第一次提交

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            string t = "";
+            int a = 0;
+            int b = 0;
+            Console.Write("Please input one number:");
+            s = Console.ReadLine();
+            Console.Write("Please input another number:");
+            t = Console.ReadLine();
+            a = Int32.Parse(s);
+            b = Int32.Parse(t);
+            a = a * b;
+            Console.WriteLine("a*b=" + a);
+
+        }
+    }
+}
---
+++ Homework1/topic5/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace topic5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string t = textBox2.Text;
+            int a = Int32.Parse(s);
+            int b = Int32.Parse(t);
+            a = a * b;
+            s = Convert.ToString(a);
+            label1.Text = s;
+        }
+    }
+}
